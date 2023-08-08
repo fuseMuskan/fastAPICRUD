93207ccac4f2360f7e0a58577bac4abda52800bb
fuseMuskan <muskan.bhandari@fusemachines.com>
2023-08-08 10:57:25 +0545

    refactor: added exception handling

--- app/main.py
+++ app/main.py
@@ -7,20 +7,27 @@ app = FastAPI()
 
 @app.on_event("startup")
 def create_table():
-    # Create all tables defined in Base's metadata
     Base.metadata.create_all(bind=engine)
 
 @app.post("/employees/")
 def create_employees(name: str, department: str, db: Session = Depends(get_db)):
-    employee = Employee(name= name, department= department)
-    db.add(employee)
-    db.commit()
-    return {"Employees added": employee.name}
+    try:
+        employee = Employee(name= name, department= department)
+        db.add(employee)
+        db.commit()
+        return {"Employees added": employee.name}
+    except:
+        raise HTTPException(status_code = status.HTTP_400_BAD_REQUEST)
+
 
 @app.get("/employees/")
 def get_employees(db: Session = Depends(get_db)):
     employees = db.query(Employee).all()
+    if not employees:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Employee not found")
     return {"status": "success", "employees": employees}
+
 
 @app.get("/employees/{employee_id}")
 def get_employees(employee_id: str, db: Session = Depends(get_db)):
@@ -30,6 +37,7 @@ def get_employees(employee_id: str, db: Session = Depends(get_db)):
                             detail=f"Employee with {employee_id} not found")
     
     return {"status_code": status.HTTP_200_OK, "employee": employee}
+
 
 @app.delete("/employees/{employee_id}")
 def delete_employee(employee_id: str, db: Session = Depends(get_db)):
@@ -43,6 +51,7 @@ def delete_employee(employee_id: str, db: Session = Depends(get_db)):
     db.commit()
     return Response(status_code = status.HTTP_204_NO_CONTENT)
 
+
 @app.put("/employees/{employee_id}/{column}/{new_value}")
 def update_employee(employee_id: str, column: str, new_value: str, db: Session = Depends(get_db)):
     employee_query = db.query(Employee).filter(Employee.id == employee_id)
@@ -52,7 +61,7 @@ def update_employee(employee_id: str, column: str, new_value: str, db: Session =
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Employee with id {employee_id} not found")
 
-    setattr(db_employee, column, new_value) # Set the new value for the specified column
+    setattr(db_employee, column, new_value)
     db.add(db_employee)
     db.commit()
     db.refresh(db_employee)
